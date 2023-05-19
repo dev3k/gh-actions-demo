63140f85b119dfb89ca9177b5d2ea4ace849614a
Khalil Majdalawi <khalil.majdalawi@gmail.com>
2023-05-19 13:42:37 +0300

    try aws upload

--- .github/actions/deploy-to-s3-javascript/main.js
+++ .github/actions/deploy-to-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  // Get Input Information
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // Upload Dist Folder
+  const s3Uri = `s3://${bucket}`;
+  // you need env var AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice("Hello From Custom Javascript Action");
 }
 
